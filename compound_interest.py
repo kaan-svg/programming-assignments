0efe4d2efb92907a5e10cb6c764ff1bd237dfaff
KAAN <piskinkaan05@gmail.com>
2026-09-25 17:11:56 +0300

    Delete compound_interest.py

--- compound_interest.py
+++
@@ -1,6 +0,0 @@
-# Compound Interest Calculator
-principle=float(input("Enter principle: "))
-rate=float(input("Enter rate (%): "))
-time=float(input("Enter time: "))
-total=principle*pow((1+rate/100),time)
-print(f"Balance after {time} years: ${total:.2f}")
