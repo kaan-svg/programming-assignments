abe8f2912f2afd37c14ed66d7d8d12f28ff2a01f
KAAN <piskinkaan05@gmail.com>
2026-09-25 17:15:08 +0300

    Delete list_comprehensions.py

--- list_comprehensions.py
+++
@@ -1,8 +0,0 @@
-# List Comprehension Examples
-doubles=[x*2 for x in range(1,11)]
-print(doubles)
-squares=[z*z for z in range(1,11)]
-print(squares)
-numbers=[1,2,-3,4,-4]
-positives=[n for n in numbers if n>=0]
-print(positives)
